144adbbe998769a08823f439efb91e033c5dee1e
fisher <yuxinyong@163.com>
2023-10-23 00:28:58 +0800

    v15

--- erpnext_chinese/public/erpnext_chinese.bundle.js
+++ erpnext_chinese/public/erpnext_chinese.bundle.js
@@ -2,14 +2,16 @@ $(document).on('page-change', function() {
     setTimeout(function(){
         if (frappe.boot.lang == 'zh' && frappe.get_route() && 
             (frappe.get_route().at(-1) == 'Workspaces' || frappe.get_route()[0] == 'Workspaces')){
-            let trans_map = [['Reports &amp; Masters', '功能报表'],
-                ["Masters & Reports",'功能报表'],
-                ['Reports & Masters', '功能报表'],
+            let trans_map = [['Reports &amp; Masters', '主数据、业务交易、报表'],
+                ["Masters & Reports",'主数据、业务交易、报表'],
+                ['Reports & Masters', '主数据、业务交易、报表'],
                 ['Elements', '定制项'],
                 ['Quick Access', '快捷方式'],
                 ['Your Shortcuts', '快捷方式'],
                 ['Settings', '设置'],
                 ['Integrations', '应用集成'],
+                ['My Workspaces', '我的工作区'],
+                ['MY WORKSPACES', '我的工作区'],
                 ['Documents', '单据']
             ];
             $.each(trans_map, function(i,v){
