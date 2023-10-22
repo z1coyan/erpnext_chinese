2d3c13f891de5b2173ee8c0a3b71954f874e7b66
fisher <yuxinyong@163.com>
2023-10-22 09:29:35 +0800

    for v15

--- erpnext_chinese/public/dist/js/erpnext_chinese.bundle.CIIANH5Q.js
+++
@@ -1,2 +0,0 @@
-(()=>{$(document).on("page-change",function(){setTimeout(function(){frappe.boot.lang=="zh"&&frappe.get_route()&&(frappe.get_route().at(-1)=="Workspaces"||frappe.get_route()[0]=="Workspaces")&&$('div.ce-block__content span b:contains("\u62A5\u8868\u4E0E\u4E3B\u6570\u636E")').html("Report and master")},1e3)});})();
-//# sourceMappingURL=erpnext_chinese.bundle.CIIANH5Q.js.map
--- erpnext_chinese/public/erpnext_chinese.bundle.js
+++ erpnext_chinese/public/erpnext_chinese.bundle.js
@@ -1,9 +1,20 @@
 $(document).on('page-change', function() {
-    setTimeout(function(){
+    //setTimeout(function(){
         if (frappe.boot.lang == 'zh' && frappe.get_route() && 
             (frappe.get_route().at(-1) == 'Workspaces' || frappe.get_route()[0] == 'Workspaces')){
-            $('div.ce-block__content span b:contains("报表与主数据")').html("Report and master");
+            let trans_map = [['Reports &amp; Masters', '功能报表'],
+                ["Masters & Reports",'功能报表'],
+                ['Reports & Masters', '功能报表'],
+                ['Elements', '定制项'],
+                ['Quick Access', '快捷方式'],
+                ['Your Shortcuts', '快捷方式'],
+                ['Settings', '设置'],
+                ['Documents', '单据']
+            ];
+            $.each(trans_map, function(i,v){
+                $('div.ce-block__content span b:contains("'+v[0]+'")').html(v[1]);
+            })
         }
-    },
-    1000)    
+    //},
+    //500)    
 });
